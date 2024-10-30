9437e46e45f3574e937b6b6cab2a6fd5ef997777
A01753505 <A01753505@tec.mx>
2024-10-30 17:36:33 -0600

    ignore para error

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from ultralytics import YOLO
+from ultralytics import YOLO    #type: ignore
 import cv2
 import base64
 import numpy as np
