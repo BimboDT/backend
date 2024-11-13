7f8fcc600f9ab812ecf0eebdc2b2b1d9a2049e91
Alfredo-Azamar <a01798100@tec.mx>
2024-11-13 16:37:02 -0600

    Changes to ConteoController

--- src/controllers/ConteoController.ts
+++ src/controllers/ConteoController.ts
@@ -23,9 +23,19 @@ class ConteoController extends AbstractController {
     protected initRoutes(): void {
         // Test endpoint
         this.router.get("/test", this.getTest.bind(this));
+
+        // Mobile App endpoints
+        this.router.get("/consultaUsuario/:numEmp", this.getSpecificEmployee.bind(this));
+        this.router.get("/consultaConteo/:fecha", this.getCycleCounting.bind(this));
+        
+        
+        this.router.post("/crearConteo", this.postCountingReport.bind(this));
+        // Web App endpoints
+
+
     }
 
-    // Test endpoint
+    // {Test endpoint}
     private getTest(req: Request, res: Response) {
         try {
             console.log("Prueba exitosa");
@@ -33,6 +43,61 @@ class ConteoController extends AbstractController {
         } catch (error: any) {
          console.log(error);
          res.status(500).send("Internal server error" + error);
+        }
+    }
+
+
+    // {Mobile App endpoints}
+    // Function that queries an employee for their ID
+    private async getSpecificEmployee(req: Request, res: Response) {
+        try {
+            const { numEmp } = req.params;
+
+            const usuario = await db.Usuario.findOne({
+                where: { NumEmpleado: numEmp },
+            });
+
+            if (usuario) {
+                res.status(200).json(usuario);
+            } else {
+                res.status(404).send("El empleado no existe");
+            }
+        } catch (error: any) {
+                console.log(error);
+                res.status(500).send("Internal server error: " + error);
+        }
+    }
+
+    private async getCycleCounting(req: Request, res: Response) {
+        try {
+            const { fecha } = req.params;
+
+            const conteo = await db.Conteo.findAll({
+                where: { Fecha: fecha },
+            });
+
+            if (conteo) {
+                res.status(200).json(conteo);
+            } else {
+                res.status(404).send("No hay conteos para esta fecha");
+            }
+        } catch (error: any) {
+            console.log(error);
+            res.status(500).send("Internal server error: " + error);
+        }
+    }
+
+
+    private async postCountingReport(req: Request, res: Response) {
+        try {
+            console.log(req.body);
+            const newCountingReport = await db.Conteo.create(req.body);
+
+            console.log("Reporte de conteo creado exitosamente");
+            res.status(201).send("<h1>Reporte creado</h1>");
+        } catch (err) {
+            console.log(err);
+            res.status(500).send("Internal server error: " + err);
         }
     }
 }
